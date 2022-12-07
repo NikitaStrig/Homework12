388c7a989db201326c3fc6500e2151d148fd8adc
nikitastrigunkov <demonkurt1995@gmail.com>
2022-12-07 14:00:05 +0300

    HW 11 task 2.

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,36 @@ public class Main {
         }
         return result;
     }
+    public static String osNew ( int clientOS, int clientDeviceYear) {
+        String resultOs = " ";
+        if (clientOS == 0) {
+            if (clientDeviceYear >= 2015) {
+                resultOs = "Установите приложения для iOS по ссылке";
+            } else {
+                resultOs = "Установите облегченную версию приложения для iOS по ссылке";
+            }
+        }
+            if (clientOS == 1) {
+                if (clientDeviceYear >= 2015) {
+                    resultOs = "Установите приложения для Android по ссылке";
+                } else {
+                    resultOs = "Установите облегченную версию приложения для Android по ссылке";
+                }
+            }
+        return resultOs;
+    }
+
+
 
     public static void main(String[] args) {
+        System.out.println("Задание 1");
         int year = 2056;
         System.out.println(yearNew(year));
+
+        System.out.println("Задание 2");
+        int clientOs = 0;
+        int clientDeviceYear = 2012;
+        System.out.println(osNew(clientOs, clientDeviceYear));
+
     }
 }
